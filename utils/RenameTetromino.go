77418ec6ecd8336fb8dc4b37d3838cd86fa2a501
lakoth <lornaakothpb@gmail.com>
2024-08-05 18:31:24 +0300

    Add:rename tetrominoes from # to corresponding alphabet ASCII

---
+++ utils/RenameTetromino.go
@@ -0,0 +1,30 @@
+package utils
+
+
+
+func RenameTetromino(tetrominoes [][]string) [][]string {
+	
+	var renamedTetrominoes [][]string
+	for i, array := range tetrominoes {
+		var renamedTetromino []string
+		var result string
+		for _, str := range array {
+			var char rune
+			for _, ch := range str {
+				if ch == '#' {
+					char = rune(i + 65)
+				} else {
+					char = ch
+				}
+				result +=(string(char))
+			}
+			renamedTetromino = append(renamedTetromino,result)
+			result=""
+		}
+
+		renamedTetrominoes = append(renamedTetrominoes, renamedTetromino)
+	
+	}
+
+	return renamedTetrominoes
+}
