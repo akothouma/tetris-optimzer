06bf6d22a5131d6552c81f0fe34ad482514f6958
lakoth <lornaakothpb@gmail.com>
2024-08-06 18:06:31 +0300

    Add:Function for initial board and printing it

---
+++ utils/PrintBoard.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+)
+
+func DrawInitialBoard(size int) [][]string {
+	initialSize := int(math.Ceil((math.Sqrt(float64(size) * 4))))
+	board := make([][]string, initialSize)
+	for row := range board {
+		board[row] = make([]string, initialSize)
+		for col := range board[row] {
+			board[row][col] = "."
+		}
+	}
+	return board
+}
+
+func PrintBoard(board [][]string) {
+	for _, row := range board {
+		for _, col := range row {
+			fmt.Print(col)
+		}
+		fmt.Println()
+	}
+}
