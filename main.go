8b10e4cfd79fa30a258533d06e0d4fdd7556621f
lakoth <lornaakothpb@gmail.com>
2024-08-08 18:50:22 +0300

    Feat:Feature to add statring board size if no solution with current board size

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
 	"tetris/utils"
 )
 
@@ -17,16 +18,28 @@ func main() {
 		return
 	}
 	if utils.NonEmptyFile(os.Args[1]) {
-		tetrominoes := utils.ExtractTetromino(os.Args[1])
+		tetrominoes, size := utils.ExtractTetromino(os.Args[1])
+		var renamedTetrominoes [][]string
+		var trimmedTetrominoes [][]string
 		if utils.IsValidTetromino(tetrominoes) {
-			var trimmedTetrominoes [][]string
+
 			for _, tetromino := range tetrominoes {
 				trimH := utils.TrimHorizontal(tetromino)
 				trimV := utils.TrimVertical(trimH)
 				trimmedTetrominoes = append(trimmedTetrominoes, trimV)
 			}
-			fmt.Println(utils.RenameTetromino(trimmedTetrominoes))
+			renamedTetrominoes = utils.RenameTetromino(trimmedTetrominoes)
 		}
+		var finalBoard [][]string
+		for {
+			board := utils.DrawInitialBoard(size)
+			finalBoard= utils.DrawFinalBoard(board, renamedTetrominoes)
+            if finalBoard!=nil{
+                 break
+			}
+            size++
+		}		
+		utils.PrintBoard(finalBoard)
 	} else {
 		fmt.Fprintln(os.Stderr, `ERROR`)
 	}
