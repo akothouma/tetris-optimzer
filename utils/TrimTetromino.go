50bd7f8781bb236cbe87be15d59c7e1c8a374384
lakoth <lornaakothpb@gmail.com>
2024-08-08 18:55:19 +0300

    Docs:documenting functions

--- utils/TrimTetromino.go
+++ utils/TrimTetromino.go
@@ -2,6 +2,7 @@ package utils
 
 import "strings"
 
+/* TrimHorizontal removes the string contents of the tertomino that have no characters in them*/
 func TrimHorizontal(tetromino []string) []string {
 	var horizontalTrimmed []string
 	for j := 0; j < len(tetromino); j++ {
@@ -19,26 +20,25 @@ func TrimHorizontal(tetromino []string) []string {
 	return horizontalTrimmed
 }
 
+/* TrimVertical checks the column in the tetromino for # and marks it as valid*/
 func TrimVertical(tetromino []string) []string {
-	
 	var i int
-	
+
 	for k := len(tetromino[0]) - 1; k >= 0; k-- {
 		columnIsValid := false
 		for j := 0; j < len(tetromino); j++ {
-			if strings.Contains(string(tetromino[j][k]),"#") {
+			if strings.Contains(string(tetromino[j][k]), "#") {
 				columnIsValid = true
 				break
 			}
 		}
-			if !columnIsValid {
-				for i=range tetromino{
-					tetromino[i]= RemoveCharAtIndex(tetromino[i], k)
-				}	
+		if !columnIsValid {
+			for i = range tetromino {
+				tetromino[i] = RemoveCharAtIndex(tetromino[i], k)
 			}
-			
 		}
 
+	}
 	return tetromino
 }
 
