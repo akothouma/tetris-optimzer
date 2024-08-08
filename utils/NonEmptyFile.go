930e46be9ca8901fbb502a92fdf8664cfa7579ce
lakoth <lornaakothpb@gmail.com>
2024-08-08 18:53:12 +0300

    Docs:documenting functions

--- utils/NonEmptyFile.go
+++ utils/NonEmptyFile.go
@@ -1,8 +1,10 @@
 package utils
 
-import "os"
-
-func NonEmptyFile(s string)bool{
-	fileStat,_:=os.Stat(s)
-    return fileStat.Size()!=0
+import (
+	"os"
+)
+/* NonEmpty ensures that sample.txt is not an empty file*/
+func NonEmptyFile(s string) bool {
+	fileStat, _ := os.Stat(s)
+	return fileStat.Size() != 0
 }
