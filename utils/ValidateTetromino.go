4a95fc28983f4b7da78ea3a82751469191a5af4a
lakoth <lornaakothpb@gmail.com>
2024-08-08 18:56:21 +0300

    documenting functions

--- utils/ValidateTetromino.go
+++ utils/ValidateTetromino.go
@@ -4,20 +4,21 @@ import (
 	"fmt"
 	"os"
 )
-/* Is ValidTetromino checks each tetromino in the tetromino slice and validates it based on number of #,connections and length of each line */
+
+/* Is ValidTetromino checks each tetromino in the tetromino slice and validates it based on number of #,connections and length of each string in the tetromino*/
 func IsValidTetromino(tetrominoes [][]string) bool {
 	for i := 0; i < len(tetrominoes); i++ {
 		var connections int
 		var count int
 		for j := 0; j < len(tetrominoes[i]); j++ {
 			for k := 0; k < len(tetrominoes[i][j]); k++ {
-				if len(tetrominoes[i][j])!=4{ //checks length of each  line of tetromino
-                    fmt.Fprintln(os.Stderr,`ERROR`)
-					return false
+				if len(tetrominoes[i][j]) != 4 { // checks length of each  line of tetromino
+					fmt.Fprintln(os.Stderr, `ERROR`)
+					os.Exit(0)
 				}
 				if tetrominoes[i][j][k] == '#' {
 					count++
-                  // check connection to neighbouring cells
+					// check connection to neighbouring cells
 					if k > 0 && tetrominoes[i][j][k-1] == '#' {
 						connections++
 					}
@@ -36,9 +37,9 @@ func IsValidTetromino(tetrominoes [][]string) bool {
 
 		if count != 4 || connections != 6 && connections != 8 {
 			fmt.Fprintln(os.Stderr, `ERROR`)
-			return false
+			os.Exit(0)
 		}
-		
+
 	}
 
 	return true
