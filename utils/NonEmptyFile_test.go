39ab1b1555820c126784e602ea7777a150eec0fc
lakoth <lornaakothpb@gmail.com>
2024-08-16 16:24:41 +0300

    Add:Test file  for NonEmptyFile  function

---
+++ utils/NonEmptyFile_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import "testing"
+
+func TestNonEmptyFile(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"sample.txt is empty",args{"sample1.txt"},false},
+		{"sample.txt is not empty",args{"sample1.txt"},true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NonEmptyFile(tt.args.s); got != tt.want {
+				t.Errorf("NonEmptyFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
