feadfc53b53e09cb432f291aed2851a88cce3ee5
lakoth <lornaakothpb@gmail.com>
2024-08-16 16:21:41 +0300

    Add:Test file  for TrimTetromino function

---
+++ utils/TrimTetromino_test.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTrimHorizontal(t *testing.T) {
+	type args struct {
+		tetromino []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"horizontally trim unwanted . to optimize tetromino shape", args{[]string{"....", "....", "....", "####"}}, []string{"####"}},
+		{"horizontally trim unwanted . to optimize tetromino shape", args{[]string{"....", ".###", ".#..", "...."}}, []string{".###", ".#.."}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TrimHorizontal(tt.args.tetromino); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TrimHorizontal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTrimVertical(t *testing.T) {
+	type args struct {
+		tetromino []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{"vertically trim unwanted . to optimize tetromino shape", args{[]string{"...#", "...#", "...#", "...#"}}, []string{"#","#","#","#"}},
+	
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TrimVertical(tt.args.tetromino); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TrimVertical() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
